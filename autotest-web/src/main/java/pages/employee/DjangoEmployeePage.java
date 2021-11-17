866827c3fe877ec17f1be22128002dc41a3a507c
Vitaly Zverev <93658720+VitalyZverev@users.noreply.github.com>
2021-11-17 11:54:33 +0300

    Bugfix

--- autotest-web/src/main/java/pages/employee/DjangoEmployeePage.java
+++ autotest-web/src/main/java/pages/employee/DjangoEmployeePage.java
@@ -37,8 +37,6 @@ public class DjangoEmployeePage extends DjangoPagesHeader {
     @Name("Предупреждение в заголовке")
     private SelenideElement headerWarning = $(".warning");
 
-    @Name("Действие")
+    @Name("Таблица чек-бокс")
     private ElementsCollection tableCheckboxes = $$(".action-select");
-
 }
-
