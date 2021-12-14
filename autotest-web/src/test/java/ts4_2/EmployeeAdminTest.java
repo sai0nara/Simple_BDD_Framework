b169d2e5462de0bcd107ec727702a1c226560840
Vitaly Zverev <alessandro_vzh@mail.ru>
2021-12-14 17:08:24 +0300

    Added test 4.2.1

---
+++ autotest-web/src/test/java/ts4_2/EmployeeAdminTest.java
@@ -0,0 +1,43 @@
+package ts4_2;
+
+import hooks.WebHooks;
+import io.qameta.allure.Description;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import ru.lanit.at.web.pagecontext.PageManager;
+import steps.WebCheckSteps;
+import steps.WebSteps;
+
+public class EmployeeAdminTest extends WebHooks {
+
+    private PageManager pageManager = new PageManager();
+    private final WebSteps webSteps = new WebSteps(pageManager);
+    private final WebCheckSteps webCheckSteps = new WebCheckSteps(pageManager);
+
+//    @DataProvider
+//    public Object[][] data() {
+//        return new Object[][]{
+//                {"admin", "Сотрудники", "Заголовок таблицы", "Выберите Сотрудник для изменения"},
+//                {"hr", "Добавить поле Сотрудники", "Заголовок таблицы", "Добавить Сотрудник"},
+//                {"public", "Сотрудники", "Заголовок таблицы", "Выберите Сотрудник для просмотра"}
+//        };
+//    }
+
+    private void initialize() {
+        webSteps.openUrl();
+        webSteps.setPage("DjangoAuthorization");
+        webSteps.authWithLogin("admin");
+        webSteps.setPage("DjangoAdministration");
+    }
+
+    @Test
+    @Description("4.1 Проверка работоспособности ссылки 'Сотрудники с административным доступом' под ролью 'admin'")
+    public void adminAccessTest() {
+        initialize();
+        webCheckSteps.checkAppearElement("Сотрудники с административным доступом");
+        webSteps.clickOnElement("Сотрудники с административным доступом");
+        webSteps.setPage("DjangoAdminAccess");
+        webCheckSteps.checkFieldText("Инфоблок", "Выберите Сотрудник с административным доступом для изменения");
+
+    }
+}
