9e54c6bde25178e32c305d73bc5da8f2ded158b3
Vitaly Zverev <alessandro_vzh@mail.ru>
2021-12-14 17:08:24 +0300

    Added test 4.2.1

---
+++ autotest-web/src/main/java/pages/employee/DjangoAdminAccessPage.java
@@ -0,0 +1,28 @@
+package pages.employee;
+
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import ru.lanit.at.web.annotations.Name;
+import ru.lanit.at.web.pagecontext.WebPage;
+
+import static com.codeborne.selenide.Selenide.$$x;
+import static com.codeborne.selenide.Selenide.$x;
+
+@Name(value = "DjangoAdminAccess")
+public class DjangoAdminAccessPage extends WebPage {
+
+    @Name("Инфоблок")
+    private SelenideElement infoblock = $x("//div[@id='content']/h1");
+    @Name("Восстановить удаленный Сотрудники с административным доступом")
+    private SelenideElement buttonRestore = $x("//a[@class='recoverlink']");
+    @Name("Добавить Сотрудник с административным доступом")
+    private SelenideElement buttonAddEmployee = $x("//a[@class='addlink']");
+    @Name("Список сотрудников")
+    private ElementsCollection listEmployee = $$x("//tbody/tr/th/a");
+    @Name("Действие")
+    private SelenideElement dropDownAction = $x("//select[@name='action']");
+    @Name("Выполнить")
+    private SelenideElement buttonExecute = $x("//button[@class='button']");
+
+
+}
