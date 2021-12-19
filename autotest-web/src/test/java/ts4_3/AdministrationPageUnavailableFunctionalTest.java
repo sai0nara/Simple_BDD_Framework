02385d8898be3e3083c09194db9b985072736cbb
Gregorio Naida <gnaida@oa-m5.msk.21-school.ru>
2021-12-19 16:43:36 +0300

    1.TextIsNotExist

--- autotest-web/src/test/java/ts4_3/AdministrationPageUnavailableFunctionalTest.java
+++ autotest-web/src/test/java/ts4_3/AdministrationPageUnavailableFunctionalTest.java
@@ -1,2 +1,45 @@
-package ts4_3;public class AdministrationPageUnavailableFunctionalTest {
+package ts4_3;
+
+import hooks.WebHooks;
+import io.qameta.allure.Description;
+import org.junit.Before;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import ru.lanit.at.web.pagecontext.PageManager;
+import steps.WebCheckSteps;
+import steps.WebSteps;
+
+
+public class AdministrationPageUnavailableFunctionalTest extends WebHooks {
+
+    private PageManager pageManager = new PageManager();
+    private final WebSteps webSteps = new WebSteps(pageManager);
+    private final WebCheckSteps webCheckSteps = new WebCheckSteps(pageManager);
+
+    @DataProvider
+    public Object[][] pageElement() {
+        return new Object[][]{
+                {"Добавить"},
+                {"Изменить"},
+                {"Сотрудники с административным доступом"},
+                {"Группы"},
+                {"Пользователи"},
+                {"TOTP devices"}
+        };
+    }
+
+    public void initialize() {
+        webSteps.openUrl();
+        webSteps.setPage("DjangoAuthorization");
+        webSteps.authWithLogin("public");
+        webSteps.setPage("DjangoAdministration");
+    }
+
+    @Test(dataProvider = "pageElement")
+    @Description("4.3.1 Проверка отсутствуют элемента {button} под ролью 'public'")
+    public void publicAccessButtons (String button)
+    {
+        initialize();
+        webCheckSteps.currentTextIsNotExist(button);
+    }
 }
