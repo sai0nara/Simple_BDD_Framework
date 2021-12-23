56daa67031740c942eebab3053271c7d8fa2495e
Archibald5 <artur.topor@mail.ru>
2021-12-23 13:42:58 +0300

    added 16.1-3 test cases and two new PageObjects in directory "administrators"

---
+++ autotest-web/src/main/java/pages/administrators/AdministratorsOfAccountPage.java
@@ -0,0 +1,24 @@
+package pages.administrators;
+
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import ru.lanit.at.web.annotations.Name;
+import ru.lanit.at.web.pagecontext.WebPage;
+
+import static com.codeborne.selenide.Selenide.*;
+
+/**
+ * Страница - Администраторы аккаунта
+ *
+ * @link - http://178.154.246.238:58082/admin/core/accountadmin/
+ */
+
+@Name("Страница - Администраторы аккаунта")
+public class AdministratorsOfAccountPage extends WebPage {
+
+    @Name("Первый сотрудник из списка")
+    private SelenideElement firstEmployeeByList = $x("*//tr[1]/th/a");
+
+    @Name("Рандомный сотрудник из списка")
+    private ElementsCollection randomEmployeeByList = $$x("//*[@id=\"result_list\"]/tbody/tr/th/a");
+}
