6b87d62b6c4795512e42afce0b92beddd0ece560
Olga Serova <serova.home@gmail.com>
2021-12-23 14:52:58 +0300

    init_ts13

---
+++ autotest-web/src/test/java/ts13/AddAdminAccountWithRoleHRPositiveTest.java
@@ -0,0 +1,12 @@
+package ts13;
+
+import hooks.WebHooks;
+import io.qameta.allure.Description;
+import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
+import ru.lanit.at.web.pagecontext.PageManager;
+import steps.WebCheckSteps;
+import steps.WebSteps;
+
+public class AddAdminAccountWithRoleHRPositiveTest {
+}
