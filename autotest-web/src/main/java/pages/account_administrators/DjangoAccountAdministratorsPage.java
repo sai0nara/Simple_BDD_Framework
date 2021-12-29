c73cc8ddc8b5ff8b18102988bdf76aa2010bfed9
AlekseyKoks-in <alekseykoksin@gmail.com>
2021-12-29 14:08:24 +0300

    Добавлены ТК 11.1-11.3

---
+++ autotest-web/src/main/java/pages/account_administrators/DjangoAccountAdministratorsPage.java
@@ -0,0 +1,47 @@
+package pages.account_administrators;
+
+import com.codeborne.selenide.ElementsCollection;
+import com.codeborne.selenide.SelenideElement;
+import ru.lanit.at.web.annotations.Name;
+import ru.lanit.at.web.pagecontext.WebPage;
+
+import static com.codeborne.selenide.Selenide.$$x;
+import static com.codeborne.selenide.Selenide.$x;
+
+/**
+ * PageObject, описывающий страницу 'Администраторы аккаунта'
+ * http://178.154.246.238:58082/admin/core/accountadmin/
+ */
+@Name(value = "DjangoAccountAdministratorsPage")
+public class DjangoAccountAdministratorsPage extends WebPage {
+    @Name("Сотрудник")
+    private ElementsCollection employeeNames = $$x("//th[@class='field-employee nowrap']/a");
+    @Name("Аккаунт")
+    private ElementsCollection accountNames = $$x("//td[@class='field-account_name nowrap']");
+    @Name("Восстановить удаленный Администраторы аккаунта")
+    private SelenideElement recoverLink = $x("//a[@class='recoverlink']");
+    @Name("Импорт")
+    private SelenideElement importLink = $x("//a[@class='import_link']");
+    @Name("Экспорт")
+    private SelenideElement exportLink = $x("//a[@class='export_link']");
+    @Name("Добавить Администратор аккаунта +")
+    private SelenideElement addEmployeeLink = $x("//a[@class='addlink']");
+    @Name("Выполнить")
+    private SelenideElement executeButton = $x("//button[@type='submit']");
+    @Name("Действие")
+    private ElementsCollection dropDownAction = $$x("//select[@name='action']/option");
+    @Name("Действие меню")
+    private SelenideElement action = $x("//select[@name='action']");
+    @Name("Количество найденных записей")
+    private SelenideElement numberRecords = $x("//p[@class='paginator']");
+    @Name("Таблица чек-бокс")
+    private ElementsCollection tableCheckboxes = $$x("//input[@class='action-select']");
+    @Name("Инфоблок")
+    private SelenideElement tableHeader = $x("//div[@id='content']/h1");
+    @Name("Выбрано 1 из")
+    private SelenideElement howMuchSearch = $x("//span[@class='action-counter']");
+    @Name("Договор")
+    private SelenideElement contractCheckbox = $x("//input[@value='contract']");
+    @Name("Формат")
+    private SelenideElement formatField = $x("//select[@name='file_format']");
+}
---
+++ autotest-web/src/main/java/pages/account_administrators/DjangoAddAccountAdministrators.java
@@ -0,0 +1,42 @@
+package pages.account_administrators;
+
+import com.codeborne.selenide.SelenideElement;
+import ru.lanit.at.web.annotations.Name;
+import ru.lanit.at.web.pagecontext.WebPage;
+
+import static com.codeborne.selenide.Selenide.$x;
+
+/**
+ * PageObject, описывающий страницу 'Добавить Администраторы аккаунта'
+ * http://178.154.246.238:58082/admin/core/accountadmin/add/
+ */
+@Name(value = "DjangoAddAccountAdministrators")
+public class DjangoAddAccountAdministrators extends WebPage {
+
+    @Name("Добавить Администратор аккаунта")
+    private SelenideElement title = $x("//div[@id = 'content']/h1");
+    @Name("Сохранить и продолжить редактирование")
+    private SelenideElement saveContinueEditing = $x("//input[@name = '_continue']");
+    @Name("Пожалуйста, исправьте ошибки ниже.")
+    private SelenideElement pleaseFixErrors = $x("//p[@class = 'errornote']");
+
+    @Name("Инфоблок сотрудник")
+    private SelenideElement employeeInfo = $x("//label[@for = 'id_employee']/../../ul");
+    @Name("Инфоблок аккаунт")
+    private SelenideElement accountInfo = $x("//label[@for = 'id_account_name']/../../ul");
+    @Name("Инфоблок дата начала")
+    private SelenideElement startDateInfo = $x("//label[@for = 'id_start_date']/../../ul");
+    @Name("Инфоблок дата окончания")
+    private SelenideElement endDateInfo = $x("//input[@id = 'id_end_date']/../../ul");
+
+
+    @Name("Сотрудник")
+    private SelenideElement selectEmployee = $x("//select[@id= 'id_employee']");
+    @Name("Дата начала")
+    private SelenideElement startDate = $x("//input[@id = 'id_start_date']");
+    @Name("Дата окончания")
+    private SelenideElement endDate = $x("//input[@id = 'id_end_date']");
+
+    @Name("Сообщение о успешном редактировании")
+    private static final SelenideElement deletedSuccess = $x("//li[@class = 'success']");
+}
