fd2250a3f51dc330294c4b6463409cd02bec9799
daskerbekov <71456808+daskerbekov@users.noreply.github.com>
2022-01-19 15:52:42 +0300

    Revert "Добавлены ТК 5.1-5.5"

--- autotest-web/src/test/java/ts5/EmployeePageTest.java
+++ autotest-web/src/test/java/ts5/EmployeePageTest.java
@@ -8,82 +8,23 @@ import steps.*;
 
 public class EmployeePageTest extends WebHooks {
 
-    private final PageManager pageManager = new PageManager();
+    private PageManager pageManager = new PageManager();
     private final WebSteps webSteps = new WebSteps(pageManager);
     private final WebCheckSteps webCheckSteps = new WebCheckSteps(pageManager);
 
-    private void authorization(String login) {
+    private void initialize() {
         webSteps.openUrl();
         webSteps.setPage("DjangoAuthorization");
-        webSteps.authWithLogin(login);
-        webCheckSteps.currentTextIsNotExist("Сообщение об ошибке");
+        webSteps.authWithLogin("hr");
         webSteps.setPage("DjangoAdministration");
-        webCheckSteps.checkAppearElement("Сотрудники");
         webSteps.clickOnElement("Сотрудники");
-        webSteps.setPage("DjangoEmployee");
-    }
-
-    @Test
-    @Description("5.1. Проверка отображения недоступного функционала на странице 'Сотрудники' " +
-            "под ролью 'public'")
-    public void checkingDisplayUnavailableFunctionality() {
-        authorization("public");
-        webCheckSteps.elementAbsentOnPage("Добавить сотрудник");
-    }
-
-    @Test
-    @Description("5.2. Проверка отсутствия активности элемента на странице 'Сотрудники' под ролью 'public'")
-    public void checkingElementIsInactive() {
-        authorization("public");
-        webSteps.clickRandom("ФИО");
-        webSteps.setPage("DjangoEmployeeChange");
-        webCheckSteps.checkElementIsReadOnly("Фамилия Public");
-
-    }
-
-    @Test
-    @Description("5.3. Проверка работоспособности поискового блока под ролью 'hr'")
-    public void checkingHealthSearchBlock() {
-        authorization("hr");
-        webSteps.fillField("Поиск", "Бородкин");
-        webCheckSteps.checkAppearElement("Найти");
-        webSteps.clickOnElement("Найти");
-        webCheckSteps.elementContainsText("ФИО", "Бородкин");
-
-    }
-
-    @Test
-    @Description("5.4. Проверка работоспособности удаления сотрудников под ролью 'hr'")
-    public void checkingHealthDeleting() {
-        authorization("hr");
-        webSteps.clickRandom("Таблица чек-бокс");
-        webCheckSteps.checkAppearElement("Выбрано 1 из 100");
-        webSteps.setAnything("Действие", 1);
-        webCheckSteps.checkAppearElement("Выполнить");
-        webSteps.clickOnElement("Выполнить");
-        webSteps.setPage("DjangoDeleteEmployee");
-        webCheckSteps.textAppearOnThePage("Вы уверены?");
-        webSteps.scrollToElement("Yes, I`m sure");
-        webSteps.clickOnElement("Yes, I`m sure");
-        webCheckSteps.textAppearOnThePage("Успешно удалены 1 Сотрудник.");
-    }
-
-    @Test
-    @Description("5.5. Проверка работоспособности экспорта списка сотрудников под ролью 'hr'")
-    public void checkingHealthListExport() {
-        authorization("hr");
-        webSteps.clickRandom("Таблица чек-бокс");
-        webCheckSteps.checkAppearElement("Выбрано 1 из 100");
-        webSteps.setAnything("Действие", 2);
-        webCheckSteps.checkAppearElement("Формат");
-        webCheckSteps.checkNoSelectedCheckbox("Договор");
-        webCheckSteps.checkAppearElement("Выполнить");
     }
 
     @Test
     @Description("5.6 страница Сотрудники, проверка работоспособности кнопок пагинации. Роль Hr")
     public void employeeHrPaginationTest() {
-        authorization("hr");
+        initialize();
+        webSteps.setPage("DjangoEmployee");
         webSteps.getElementText("ФИО", 1);
         webSteps.clickOnElementWithText("Пагинация", "2");
         webCheckSteps.checkCurrentNumber("Пагинация текущая страница", "2");
@@ -93,7 +34,8 @@ public class EmployeePageTest extends WebHooks {
     @Test
     @Description("5.7 страница Сотрудники, проверка работоспособности восстановления удаленных сотрудников. Роль Hr")
     public void employeeHrRestoreTest() {
-        authorization("hr");
+        initialize();
+        webSteps.setPage("DjangoEmployee");
         webSteps.getElementText("ФИО", 1);
         webSteps.clickOnElementWithText("Пагинация", "2");
         webCheckSteps.checkCurrentNumber("Пагинация текущая страница", "2");
@@ -103,7 +45,8 @@ public class EmployeePageTest extends WebHooks {
     @Test
     @Description("5.9 страница Сотрудники, проверка работоспособности экспорта списка сотрудников через кнопку интерфейса. Роль Hr")
     public void employeeHrExportTest() {
-        authorization("hr");
+        initialize();
+        webSteps.setPage("DjangoEmployee");
         webCheckSteps.checkAppearElement("Экспорт");
         webSteps.clickOnElement("Экспорт");
         webSteps.setPage("DjangoExportEmployee");
@@ -116,7 +59,8 @@ public class EmployeePageTest extends WebHooks {
     @Test
     @Description("5.10 страница Сотрудники, проверка работоспособности кнопки добавления нового сотрудника. Роль Hr")
     public void employeeHrAddNewEmployeeTest() {
-        authorization("hr");
+        initialize();
+        webSteps.setPage("DjangoEmployee");
         webCheckSteps.checkAppearElement("Добавить сотрудник");
         webSteps.clickOnElement("Добавить сотрудник");
         webSteps.setPage("DjangoExportEmployee");
@@ -126,7 +70,8 @@ public class EmployeePageTest extends WebHooks {
     @Test
     @Description("5.11 страница Сотрудники, проверка работоспособности фильтров. Роль Hr")
     public void employeeHrFiltersTest() {
-        authorization("hr");
+        initialize();
+        webSteps.setPage("DjangoEmployee");
         webSteps.clickOnElementWithText("Категории фильтров", "Офис");
         webSteps.clickOnElementWithText("Фильтр", "Пенза");
         webCheckSteps.checkCurrentNumberCity("Текущий город", "Пенза");
